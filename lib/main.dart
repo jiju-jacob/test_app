87021818ec4f299b2b6532ef49430e9a09886cee
Jiju S Jacob <jiju@keyvalue.systems>
2024-06-10 17:51:44 +0530

    Updated title in main file to full name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Jiju',
+      title: 'Jiju S Jacob',
       theme: ThemeData(
         // This is the theme of your application.
         //
